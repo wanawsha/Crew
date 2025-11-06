aed8057921422bee972c3bb0c2ad5c4a6e293a61
wanawsha <wanawshaak@gmail.com>
2025-11-06 18:10:59 +0100

    Installed Tailwind correctly

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./*html",
+        "./pages/**/*html",
+        "./js/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
